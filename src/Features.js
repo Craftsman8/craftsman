c7c59a714bbfb81cf1531e9b1473493cfc014be3
Craftsman8 <Craftsman8@users.noreply.github.com>
2024-01-18 01:36:41 +0100

    Adds new features with icon

---
+++ src/Features.js
@@ -0,0 +1,18 @@
+// import { FaPaintBrush } from "react-icons/fa";
+// <FaPaintBrush />
+
+import { GiPencilBrush } from "react-icons/gi";
+const Features = () => {
+    return ( 
+        <div className="newFeatures">
+        <center>
+        <div class="vl"></div>
+        <h1> <GiPencilBrush className="brush"/> CRAFTSMAN </h1>
+        <h5> Arts & Graphics </h5>
+        </center>
+        </div>
+        
+     );
+}
+ 
+export default Features;
