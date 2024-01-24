a1d5182a0a4b9c56dfbbe54146a73c99bda9a8fb
Craftsman8 <Craftsman8@users.noreply.github.com>
2024-01-24 17:36:10 +0100

    Applying DeleteButton

--- src/Components.js
+++ src/Components.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import DeleteButton from "./DeleteButton";
 
 const Components = () => {
     const [components, setComponents] = useState(
@@ -34,23 +35,13 @@ const Components = () => {
     otherServices()
 
     const handleDelete = (id) => {
-        const filteredComponents = components.filter((component, index) => components.id !== id )
+        console.log('The id of the clicked component is', id)
+        const filteredComponents = components.filter((component, index) => component.id !== id )
         setComponents(filteredComponents)
     }
-
+    
     return ( 
-        <div className="component">
-            <center>
-            <h2>Top Professionals</h2>
-            {components.map((component, index)=>(
-                <div className="component-slide" key={component.id}>
-                    <h2>{component.title}</h2>
-                    <p>By: {component.author}</p>
-                    <button onClick={() => {handleDelete(component.id)}}>Delete</button>
-                </div>
-            ))}
-            </center>
-        </div>
+        <DeleteButton components={components} handleDelete={handleDelete}/>
      );
 }
  
