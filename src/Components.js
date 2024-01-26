a9a896dc1cc0b703187f3c5f4aa226faef865a0b
Craftsman8 <Craftsman8@users.noreply.github.com>
2024-01-26 22:16:36 +0100

    Add error checking functions

--- src/Components.js
+++ src/Components.js
@@ -3,19 +3,32 @@ import DeleteButton from "./DeleteButton";
 
 const Components = () => {
     const [components, setComponents] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     const [course, setCourse] = useState("> Frontend <");
 
     useEffect(() => {
-        fetch("http://localhost:8000/courses")
-        .then((response)=>{
-            return response.json()
-        })
-        .then((componentArray)=>{
-            console.log(componentArray)
-            setComponents(componentArray)
-        })
-    }, []); //dependency array
+        setTimeout(()=>{
+            fetch("http://localhost:8000/coursess")
+            .then((response)=>{
+                if (!response.ok){
+                    throw Error("Ooooooooopss! ...unable to connect to the server. >>>Check your internet connection and try again!")
+                }
+                return response.json()
+            })
+            .then((item)=>{
+                console.log(item)
+                setComponents(item)
+                setLoading(false)
+            })
+            .catch((error)=>{
+                console.log(error.message)
+                setError(error.message)
+                setLoading(false)
+            })
+        }, 5000)
+    }, []); //empty dependency array - allows the app to run only once
 
     const handleDelete = (id) => {
         console.log('The id of the clicked component is', id)
@@ -26,6 +39,10 @@ const Components = () => {
     return ( 
         <div className="Components">
             <h2>My Courses</h2>
+            {loading && <div className="loading">Fetching from server...<br></br>This might take some seconds</div> }
+            
+            {error && <div className="error">{error}</div>}
+
         {components && <DeleteButton components={components} handleDelete={handleDelete}/>}
         <button onClick={()=>setCourse("> Backend <")}>change course</button>
         
